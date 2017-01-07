993a222fe7083bff866968c253ae4f39eb7eab3c
eloylp <eloy@sandboxwebs.com>
2017-01-07 10:54:56 +0100

    added types to docs

--- roadwarrior/mediators/patrol.py
+++ roadwarrior/mediators/patrol.py
@@ -3,6 +3,11 @@ from threading import Thread
 
 class Patrol(Thread):
     def __init__(self, communications_service, engine_service, sensors_service):
+        """
+        :type communications_service: services.communications.service.CommunicationsService
+        :type engine_service: services.engine.service.EngineService
+        :type sensors_service: services.sensors.service.SensorsService
+        """
         super(Patrol, self).__init__()
         self.communications_service = communications_service
         self.engine_service = engine_service
@@ -10,4 +15,6 @@ class Patrol(Thread):
 
     def run(self):
         # TODO implement mediation between sevices.
+
+
         pass
