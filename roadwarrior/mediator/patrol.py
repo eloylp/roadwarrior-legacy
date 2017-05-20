5b1aa65a7e53da4369f9bce2a1108de7f6b37bb8
eloylp <eloy@sandboxwebs.com>
2017-05-21 00:40:56 +0200

    refactor comment types.

--- roadwarrior/mediator/patrol.py
+++ roadwarrior/mediator/patrol.py
@@ -2,19 +2,15 @@ from threading import Thread
 
 
 class Patrol(Thread):
-    def __init__(self, communications_service, engine_service, sensors_service):
+    def __init__(self, engine_service, sensors_service):
         """
-        :type communications_service: services.communications.service.CommunicationsService
-        :type engine_service: services.engine.service.EngineService
-        :type sensors_service: services.sensors.service.SensorsService
+        :type engine_service: roadwarrior.device.engine.service.EngineService
+        :type sensors_service: roadwarrior.device.sensor.service.UltrasonicSensorService
         """
         super(Patrol, self).__init__()
-        self.communications_service = communications_service
         self.engine_service = engine_service
         self.sensors_service = sensors_service
 
     def run(self):
         # TODO implement mediation between sevices.
-
-
         pass
