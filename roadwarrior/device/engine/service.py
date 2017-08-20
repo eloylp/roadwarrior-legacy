ff873aad5c77b430727614687bf057752b98f26c
eloylp <eloy@sandboxwebs.com>
2017-08-21 00:17:19 +0200

    improving code read.

--- roadwarrior/device/engine/service.py
+++ roadwarrior/device/engine/service.py
@@ -6,7 +6,7 @@ class EngineService(object):
 
     def process(self, command):
         if command[0] in self.move.keys():
-            if len(command) == 2:
+            if len(command) is 2:
                 self.move[command[0]].execute(*command[1])
             else:
                 self.move[command[0]].execute()
