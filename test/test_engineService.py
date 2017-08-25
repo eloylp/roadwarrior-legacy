d343ec08a2da7a27b7a1682ed4d0da95a98c66b2
eloylp <eloy@sandboxwebs.com>
2017-08-25 23:14:38 +0200

    reformat imports

--- test/test_engineService.py
+++ test/test_engineService.py
@@ -1,9 +1,7 @@
 # coding=utf-8
 
 from unittest import TestCase
-
 from mock import Mock
-
 from roadwarrior.device.engine.service import EngineService
 
 
@@ -28,4 +26,3 @@ class TestEngineService(TestCase):
         self.sut.process(("MOVE1", (1, 2)))
         self.move1.execute.assert_called_with(1, 2)
         self.move2.execute.assert_not_called()
-
