6eed33cd4f353ef6da7c942a244b28035771258f
eloylp <eloy@sandboxwebs.com>
2017-09-01 18:51:29 +0200

    refacto factory.

--- roadwarrior/factory/patrol.py
+++ roadwarrior/factory/patrol.py
@@ -1,15 +1,13 @@
 # coding=utf-8
 
-import inspect
-
-import roadwarrior
-from roadwarrior.device.engine.navigation import TargetHeadingCalculator
-from roadwarrior.device.sensor.definition import Sensors
-from roadwarrior.device.engine.engine import EngineFactory
-from roadwarrior.device.sensor.sensor import SensorFactory
-from roadwarrior.device.engine.service import EngineService
-from roadwarrior.device.sensor.service import SensorService
+from device.engine.move import AllStopMove, AllForwardMove, AllBackwardMove, TurnDegreesMove
 from roadwarrior.behaviour.patrol import Patrol
+from roadwarrior.device.engine.engine import EngineFactory
+from roadwarrior.device.engine.navigation import TargetHeadingCalculator
+from roadwarrior.device.engine.service import EngineService
+from roadwarrior.device.sensor.definition import Sensors
+from roadwarrior.device.sensor.sensor import SensorFactory
+from roadwarrior.device.sensor.service import SensorService
 
 
 class PatrolFactory(object):
@@ -24,14 +22,15 @@ class PatrolFactory(object):
 
         sensors_service = SensorService(sensors)
 
-        movements = {}
-        for movement in inspect.getmembers(roadwarrior.device.engine.move, predicate=inspect.isclass):
-            if 'Turn' in movement[0]:
-                movements[movement[0]] = movement[1](
-                    engines,
-                    sensors_service.get_sensor_by_key(Sensors.COMPASS),
-                    TargetHeadingCalculator()
-                )
+        movements = {
+
+            AllStopMove.__name__: AllStopMove(engines),
+            AllForwardMove.__name__: AllForwardMove(engines),
+            AllBackwardMove.__name__: AllBackwardMove(engines),
+            TurnDegreesMove.__name__: TurnDegreesMove(engines, sensors_service.get_sensor_by_key(Sensors.COMPASS),
+                                                      TargetHeadingCalculator()),
+        }
+
         engine_service = EngineService(movements)
 
         patrol = Patrol(sensors_service, engine_service)
